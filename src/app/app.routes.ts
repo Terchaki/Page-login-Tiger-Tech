287fcf9eee8175a212cf18c5b2286a2dc77f8d06
Lucas <lucas.hsm15@hotmail.com>
2025-06-05 17:02:30 -0300

    controls forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,5 +15,6 @@ export const routes: Routes = [
   {
     path: '**',
     redirectTo: '',
+    pathMatch: 'full',
   },
 ];
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,14 +1,101 @@
 import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { SpinnerComponent } from '../../shared/spinner/spinner.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, SpinnerComponent],
+  imports: [
+    FormsModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    SpinnerComponent,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   standalone: true,
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form: FormGroup;
+  submitForm: boolean = false;
+  spinner: boolean = false;
+
+  constructor(private formBuilder: FormBuilder) {
+    /**
+     * Inicializando forms
+     */
+    this.form = this.formBuilder.group(this.validatorsForm());
+  }
+
+  /**
+   * Controles de validacao do formulário.
+   */
+  validatorsForm() {
+    return {
+      /**
+       * Validadores do form:
+       */
+      username: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(50),
+        ]),
+      ],
+      password: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(10),
+          (control) => {
+            const value = control.value || '';
+            const errors: any = {};
+            if (value.length < 10) {
+              errors.minLength = true;
+            }
+            if (!/[a-zA-Z]/.test(value)) {
+              errors.letter = true;
+            }
+            if (!/[0-9]/.test(value)) {
+              errors.number = true;
+            }
+            if (!/[^a-zA-Z0-9]/.test(value)) {
+              errors.symbol = true;
+            }
+            return Object.keys(errors).length ? errors : null;
+          },
+        ]),
+      ],
+    };
+  }
+
+  /**
+   * Controls do formulário.
+   */
+  get f() {
+    return this.form.controls;
+  }
+
+  onSubmit() {
+    console.log(this.form);
+    console.log(this.form.valid);
+    this.submitForm = true;
+    if (this.form.valid) {
+      this.spinner = true;
+      setTimeout(() => {
+        this.spinner = false;
+      }, 5000);
+    }
+  }
+}
